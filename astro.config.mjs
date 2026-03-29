253e13e7c391fe05871d265e07813793f1e4ddb3
Pete Witcher <pete.witcher@gmail.com>
2026-03-29 12:40:36 -0600

    deployment action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import starlight from '@astrojs/starlight';
 import starlightOpenAPI from 'starlight-openapi'; // 1. Import the plugin
 
 export default defineConfig({
+  site: 'https://pwitcher.github.io', // Replace 'pwitcher' with your actual GH username
+  base: 'https://github.com/pwitcher/far-force',           // Replace with your exact GitHub repository name
+  
   integrations: [
     starlight({
-      title: 'Pete Witcher | ',
+      title: 'Pete Witcher | Senior Technical Writer and Documentation Engineer',
       plugins: [
         // 2. Add the plugin configuration
         starlightOpenAPI([
