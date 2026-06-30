eea31b3b0bfd1da0aa93f86fa892f9a7ba661583
Pete Witcher <pete.witcher@gmail.com>
2026-06-29 19:04:14 -0600

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightOpenAPI from 'starlight-openapi';
 
 export default defineConfig({
   site: 'https://www.petewitcher-techwriter.com',
@@ -9,46 +8,12 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Pete Witcher | Senior Technical Writer and Documentation Engineer',
-      plugins: [
-        starlightOpenAPI([
-          {
-            base: 'api-docs',
-            label: 'Songbook API',
-            schema: './openapi.yaml', 
-          },
-        ]),
-      ],
       sidebar: [
         { label: 'Home', link: '/' },
-        
+
         // Converted to flat, top-level links
         { label: 'Writing samples', link: '/writing-samples/writing-samples/' },
         { label: 'Skill examples', link: '/skill-examples/skill-examples-overview/' },
-        
-        {
-          label: 'API documentation sample',
-          items: [
-            {
-              label: 'Overview', link: '/api-docs/'
-            },
-            {
-              label: 'View source (GitHub)',
-              link: 'https://github.com/pwitcher/songlistApi'
-            },
-            {
-              label: 'All songs',
-              link: '/api-docs/operations/getallsongs/'
-            },
-            {
-              label: 'Find by title',
-              link: '/api-docs/operations/getsongbytitle/'
-            },
-            {
-              label: 'Advanced search',
-              link: '/api-docs/operations/searchsongs/'
-            },
-          ],
-        },
       ],
     }),
   ],
